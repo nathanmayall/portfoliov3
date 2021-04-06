9ef7c7ac60feb66971e69e04aa287b9ef04383a9
Nathan Mayall <70609396+nathanmayall@users.noreply.github.com>
2021-04-06 21:50:09 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        Skobeloff: "#297373",
+        TailwindLogo: "#43a8b3",
+        ReactLogo: "#00D8FF",
+        html5: "#E44D26",
+        javascript: "#f7df1e",
+        typescript: "#007acc",
+        postgresql: "#336791",
+        nodejs: "#68A063",
+        webpack: "#1C78C0",
+        kubernetes: "#326ce5",
+      },
+      backgroundImage: {
+        "bio-image": "url('./img/paintsplash.jpg')",
+      },
+    },
+  },
+  variants: {},
+  plugins: [require("tailwindcss-textshadow")],
+};
