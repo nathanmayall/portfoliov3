65f34295599f0348d5efa66a292fc0c8ed0cde26
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-04-07 22:31:31 +0100

    add title

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Title from "../components/Title";
 import Body from "../components/Body";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Nathan Mayall's Portfolio. Hi!</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Title />
       <Body />
       <Footer />
