3237fd1e94a2ec07cf8a53da27f2dbcb08865c0f
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-06-01 21:12:57 +0100

    add analytics script

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
         <title>Nathan Mayall's Portfolio. Hi!</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <script async defer data-domain="nathanmayall.com" src="https://stats.nathanmayall.com/js/plausible.js"></script>
       </Head>
       <Title />
       <Body />
