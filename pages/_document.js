30e3df97227b6ef604c6be9b141d6ae51ee6784f
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-06-17 10:55:05 +0100

    add analytics script for cloudflare

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,8 @@ class MyDocument extends Document {
             data-domain="nathanmayall.com"
             src="https://stats.nathanmayall.com/js/plausible.js"
           />
+          <Script src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "1f4d556aff2e42429eb59b3ef770906f"}'
+          />
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <body className="font-body">
