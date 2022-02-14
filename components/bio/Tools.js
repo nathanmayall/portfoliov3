07cb11c4da9990a665debd6c2a01d8b39c641d66
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2022-02-14 19:57:37 +0000

    update to tailwindv3 and next 12

--- components/bio/Tools.js
+++ components/bio/Tools.js
@@ -1,11 +1,11 @@
-import { SiKubernetes, SiNodeDotJs, SiWebpack } from "react-icons/si";
+import { SiKubernetes, SiNodedotjs, SiWebpack } from "react-icons/si";
 
 export default function Tools() {
   return (
     <div className="p-8 duration-150 bg-white rounded-lg shadow-lg bg-opacity-40 w-96 backdrop-filter backdrop-blur-sm hover:backdrop-blur-md">
       <h2 className="text-2xl text-shadow-sm">Tools</h2>
       <div className="flex justify-between mx-4 my-3 text-6xl">
-        <SiNodeDotJs className="text-nodejs" />
+        <SiNodedotjs className="text-nodejs" />
         <SiWebpack className="text-webpack" />
         <SiKubernetes className="text-kubernetes" />
       </div>
